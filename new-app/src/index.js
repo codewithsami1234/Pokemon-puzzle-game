c3ef4f2b7fd203b774c50e608d5d91e30b0c17a2
Rahma Shahbaz <samisk3232@gmail.com>
2025-09-29 23:12:49 -0700

    Update index.js

--- new-app/src/index.js
+++ new-app/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div className="App">
+  <HashRouter>
     <App />
-  </div>
+  </HashRouter>
 );
+
 reportWebVitals();
